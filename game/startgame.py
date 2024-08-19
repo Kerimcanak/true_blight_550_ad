8eea20b838610a7bb2de2014147d1b7f42e2a88d
Kerimcan AK <kerimcanak@gmail.com>
2024-08-20 00:32:00 +0300

    Characters diff aprroach

--- game/startgame.py
+++ game/startgame.py
@@ -8,39 +8,6 @@ print("You arrive on a random grove in northern Italy and the warband seem to ha
 time.sleep(1)
 print("*crow sound*")
 time.sleep(1)
-print("You wake up next to a river, it seems like no one is around except for you and your horse.")
+print("You wake up in a dream, the divine figure Wudanaz asks your identity.")
 time.sleep(1)
-name = input("Name: ")
-player_stats["name"] = name
-print(f"{name}! You are still alive! Thank Wudanaz! Now I know I am safe, let’s go buddy, c’mon.")
-time.sleep(1)
-print("A figure appears on your back, a man in red Roman robes holding a sword, slightly fat with a mustache:")
-time.sleep(1)
-print("'Who are you, stranger? Why are you in our village?'")
-time.sleep(1)
-print(f"{name}! I am {name}! I am of Heruli tribe.")
-time.sleep(1)
-religion_selection = input("A: 'I am a man.' B: 'I am a woman.' C: 'I am one with nature.' >> ")
-if religion_selection.lower() == "a":
-    time.sleep(1)
-    print("'A warrior? Then, your fate is determined.'")
-    time.sleep(1)
-    player_stats["weapon"] = "Sword"
-    player_stats["profession"] = "Warrior"
-elif religion_selection.lower() == "b":
-    time.sleep(1)
-    print("'A shield-bearer? May love protect you.'")
-    time.sleep(1)
-    player_stats["weapon"] = "Shield"
-    player_stats["profession"] = "Shield-bearer"
-elif religion_selection.lower() == "c":
-    time.sleep(1)
-    print("'A shaman? Then, you already know it.'")
-    time.sleep(1)
-    player_stats["weapon"] = "Feathered Spear"
-    player_stats["profession"] = "Shaman"
-else:
-    print("Invalid input. Game over.")
-    exit()
-time.sleep(1)
-print(player_stats)
+print("Who you are?")
